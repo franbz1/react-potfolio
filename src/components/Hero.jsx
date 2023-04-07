73a3199cfde84d2cd9515ef888c493ddc9107de5
Fran <franciscorualesp15@gmail.com>
2023-04-06 23:07:18 -0500

    modelo earth completo y seccion contact implementada

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
+    <section id='about' className='relative w-full h-screen mx-auto'>
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
