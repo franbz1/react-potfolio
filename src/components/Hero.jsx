af690c3674a381b744886fa65e96ec92dcc3d58e
Fran <franciscorualesp15@gmail.com>
2023-04-20 17:48:40 -0500

    cambio al hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,42 @@
 import { motion } from 'framer-motion'
 import { styles } from '../style'
 import { ComputersCanvas } from './canvas'
+import { github } from '../assets'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <section id='about' className='relative w-full h-screen mx-auto'>
+    <section
+      id='about'
+      className='relative w-full h-[75vh] sm:h-screen mx-auto'
+    >
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-col sm:flex-row items-center gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        <div className='flex'>
+          <div className='flex flex-col justify-center items-center mr-5'>
+            <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+            <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          </div>
+          <div className='flex flex-col'>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hi, I'm <span className='text-[#915eff]'>Miguel</span>
+            </h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              {' '}
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.{' '}
+              <br className='sm:block hidden' /> Esse error eligendi temporibus
+              accusamus neque nihil odio
+            </p>
+          </div>
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915eff]'>Miguel</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            {' '}
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.{' '}
-            <br className='sm:block hidden' /> Esse error eligendi temporibus
-            accusamus neque nihil odio
-          </p>
+          <img
+            src={github}
+            alt=''
+          />
         </div>
       </div>
-      <ComputersCanvas />
     </section>
   )
 }
