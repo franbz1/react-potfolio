ed84ff9d335522bf00f7e977420a2a0f044ec120
Fran <franciscorualesp15@gmail.com>
2023-04-21 13:49:15 -0500

    optimizacion imagenes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,15 @@
 import logo from "./logo.svg";
 import linkedin from "./linkedin.svg";
-import github from "./github.png";
+import github from "./github.avif";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
 import warriorsgym from "./company/warriorgym.svg";
-import freelancer from "./company/freelancer.png";
+import freelancer from "./company/freelancer.avif";
 
-import geritch from "./react-proyect2.png";
-import gpt3 from "./react-proyect1.png";
-import springCrud from "./crud-spring.png";
+import geritch from "./react-proyect2.avif";
+import gpt3 from "./react-proyect1.avif";
+import springCrud from "./crud-spring.avif";
 
 export {
   logo,
--- src/components/index.js
+++ src/components/index.js
@@ -5,12 +5,10 @@ import Tech from './Tech';
 import Experience from './Experience';
 import Works from './Works';
 import Contact from './Contact';
-import LaptopPage from "./LaptopPage";
 
 export {
   StarsCanvasv2,
   Hero,
-  LaptopPage,
   Navbar,
   Tech,
   Experience,
